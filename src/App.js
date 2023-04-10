b4646a11ad3411cee4c12dc9360968423a06ab11
bilyos <melimerveil@gmail.com>
2023-04-10 16:29:03 +0200

    portfolio project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+import Intro from "./components/Intro/Intro";
+import Services from "./components/Services/Services";
+import Experience from "./components/Experience/Experience";
+import Works from "./components/Work/Works";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonial from "./components/Testimonial/Testimonial";
+import Contact from "./components/Contact.jsx/Contact";
+import Footer from "./components/Footer/Footer";
+import { themeContext } from "./Context"
+import {useContext} from "react"
+
 function App() {
+  const theme  = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
-      Subscribe ZainKeepsCode
+    <div className="App" style={{background: darkMode? 'black': '', color: darkMode? 'white': ''}}>
+
+      <Navbar />
+      <Intro/>
+      <Services/>
+      <Experience/>
+      <Works/>
+      <Portfolio/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
-
 export default App;
